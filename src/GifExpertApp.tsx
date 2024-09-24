ae254e4b6724efa5ffd51f75a60f585d7a53e590
Julia Sanchis <jursanchis@gmail.com>
2024-09-24 18:06:32 +0200

    restyling

--- src/GifExpertApp.tsx
+++ src/GifExpertApp.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { AddCategory, GritGrid } from "./components";
+import { BsFiletypeGif } from "react-icons/bs";
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["Adventure Time"] as string[]);
@@ -15,7 +16,9 @@ export const GifExpertApp = () => {
 
   return (
     <>
-      <h1>GifExpertApp</h1>
+      <h1>
+        <BsFiletypeGif /> GifExpertApp
+      </h1>
 
       <AddCategory
         onNewCategory={(value: string) => {
